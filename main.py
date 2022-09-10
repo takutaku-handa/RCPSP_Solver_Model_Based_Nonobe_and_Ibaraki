52cd1227d72a76a6fd42a5ff66cfa3b313ce1e42
takutaku-handa <takutaku.handa27@gmail.com>
2022-09-11 04:50:18 +0900

    CONSTRUCTが思うように動作するところまで。 CONSTRUCTの計算速度は、今のところ 10000回/sec ぐらい。

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ class Mode:
     def __init__(self, name="unknown mode"):
         self.name = name
         self.resource = {}
+        self.duration = None
 
     def addResource(self, name: str, amount: np.array):
         if name not in self.resource.keys():
             self.resource[name] = amount
+
+    def setDuration(self, duration: int):
+        self.duration = duration
 
 
 class Job:
@@ -66,7 +70,6 @@ class Model:
                 rbj = self.res_by_job[job_name]
                 rbj.append(res)
 
-
         else:
             print(f"同じ名前のモード({mode.name})が存在します")
             return
@@ -106,64 +109,115 @@ class Model:
         completion_time = {}
         start_time = {}
         i = 0
+        t_bar = 0
         remain_resource = {}
         immediate_consts = {}
         for rs_key, rs_value in self.resource.items():
             remain_resource[rs_key] = rs_value.max
             immediate_consts[rs_key] = [imm[0: 2] for imm in self.immediate if imm[2] == rs_key]
 
-        while i >= len(job_list):
-            job = job_list[i]
-            mode = mode_list[i]
-            precedence = [pre for pre in self.precedence if pre[1] == job and pre[1] in completion_time.keys()]
-            immediate = [imm for imm in self.immediate if imm[1] == job and imm[1] in completion_time.keys()]
-            t_bar = max([0] + [value for key, value in completion_time.items()
-                               if key in precedence or key in immediate])
+        step4_flag = False
+
+        while i < len(job_list):
+            now_job = job_list[i]
+            now_mode = mode_list[i]
+            precedence = [pre[0] for pre in self.precedence if pre[1] == now_job and pre[0] in completion_time.keys()]
+            immediate = [imm[0] for imm in self.immediate if imm[1] == now_job and imm[0] in completion_time.keys()]
+            all_precedence = [pre for pre in self.precedence if pre[0] in completion_time.keys()]
+            all_immediate = [imm for imm in self.immediate if imm[0] in completion_time.keys()]
+            for all_imm in all_immediate:
+                all_precedence.append(all_imm[0: 2])
+
+            if step4_flag:
+                v = [value for key, value in completion_time.items() if key in precedence or key in immediate]
+                if v:
+                    t_bar = max(v)
+                else:
+                    t_bar = 0
+            step4_flag = False
+
             T = 0
             solved = False
-            pmj = 0
+            pmj = self.job[now_job].modes[now_mode].duration
 
-            for res_name in self.res_by_job[job]:
+            for res_name in self.res_by_job[now_job]:
                 limit_immediate_upper = 0
                 limit_immediate_lower = "inf"
 
-                usage = self.job[job].modes[mode].resource[res_name]
-                length = len(usage)
-                pmj = max(pmj, length)
+                usage = self.job[now_job].modes[now_mode].resource[res_name]
 
                 for imm in immediate_consts[res_name]:
-                    if limit_immediate_lower == "inf":
-                        limit_immediate_lower = completion_time[imm[0]] - 1
-                    else:
-                        limit_immediate_lower = min(limit_immediate_lower, completion_time[imm[0]] - 1)
-                    limit_immediate_upper = max(limit_immediate_upper, start_time[imm[1]])
+                    if imm[0] in completion_time.keys():
+                        if limit_immediate_lower == "inf":
+                            limit_immediate_lower = completion_time[imm[0]] - 1
+                        else:
+                            limit_immediate_lower = min(limit_immediate_lower, completion_time[imm[0]] - 1)
+                    if imm[1] in start_time.keys():
+                        limit_immediate_upper = max(limit_immediate_upper, start_time[imm[1]])
+
+                if limit_immediate_lower == "inf":
+                    limit_immediate_lower = -1
 
                 if t_bar <= limit_immediate_lower:
                     t_list = list(range(t_bar, limit_immediate_lower + 1)) + list(range(limit_immediate_upper, max_t))
                 else:
-                    t_list = list(range(limit_immediate_upper, max_t))
+                    t_list = list(range(max(t_bar, limit_immediate_upper), max_t))
+
                 for t in t_list:
-                    after = remain_resource[res_name][t: t + length] - usage
+                    after = remain_resource[res_name][t: t + pmj] - usage
                     if np.all(after >= 0):
-                        T = t
+                        T = max(T, t)
                         solved = True
                         break
                 if not solved:
                     return "failure"
 
-            for imm in immediate:
-                for j_dash in self.job_by_res[imm[2]]:
-                    if completion_time[imm[0]] <= start_time[j_dash] < T:
-                        if [imm, j_dash] in precedence:
-                            i = job_list.index(j_dash)
-                            t_bar = T
-                        else:
-                            i = job_list.index(imm)
-                            t_bar = start_time[j_dash] + 1 - pmj
-                        return
+            for res_name in self.res_by_job[now_job]:
+                usage = self.job[now_job].modes[now_mode].resource[res_name]
+                after = remain_resource[res_name][T: T + pmj] - usage
+                remain_resource[res_name][T: T + pmj] = after
+                start_time[now_job] = T
+                completion_time[now_job] = T + pmj
+
+            back_tracking_flag = False
+            for imm in all_immediate:
+                if imm[1] == now_job:
+                    for j_dash in self.job_by_res[imm[2]]:
+                        if imm[0] != j_dash and imm[0] in completion_time.keys() and j_dash in start_time.keys():
+                            if completion_time[imm[0]] <= start_time[j_dash] < T:
+                                back_tracking_flag = True
+                                if [imm[0], j_dash] in all_precedence:
+                                    i = job_list.index(j_dash)
+                                    t_bar = T
+                                    index = job_list.index(j_dash)
+                                else:
+                                    i = job_list.index(imm[0])
+                                    imm_0_mode_name = mode_list[job_list.index(imm[0])]
+                                    dur = self.job[imm[0]].modes[imm_0_mode_name].duration
+                                    t_bar = start_time[j_dash] + 1 - dur
+                                    index = job_list.index(imm[0])
+
+                                rest_jobs = job_list[index:]
+                                rest_modes = mode_list[index:]
+                                for rest_j, rest_m in zip(rest_jobs, rest_modes):
+                                    if rest_j in start_time.keys():
+                                        for res_name in self.res_by_job[rest_j]:
+                                            usage = self.job[rest_j].modes[rest_m].resource[res_name]
+                                            start_rest = start_time[rest_j]
+                                            rest_dur = self.job[rest_j].modes[rest_m].duration
+                                            after = remain_resource[res_name][start_rest: start_rest + rest_dur] + usage
+                                            remain_resource[res_name][start_rest: start_rest + rest_dur] = after
+                                        del start_time[rest_j]
+                                        del completion_time[rest_j]
+                                break
                     else:
-                        start_time[job] = T
-                        completion_time[job] = T + pmj
-                        i += 1
+                        continue
+                    break
+
+            if not back_tracking_flag:
+                start_time[now_job] = T
+                completion_time[now_job] = T + pmj
+                i += 1
+                step4_flag = True
 
         return start_time
--- sub.py
+++ sub.py
@@ -7,34 +7,31 @@ model = Model()
 machine1 = Resource("machine1")
 machine2 = Resource("machine2")
 machine_dummy = Resource("dummy_machine")
-machine1.setMax(np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
-machine2.setMax(np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
-machine_dummy.setMax(np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+machine1.setMax(np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+machine2.setMax(np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+machine_dummy.setMax(np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
 model.addResource(machine1)
 model.addResource(machine2)
 model.addResource(machine_dummy)
 
 # job
-for i in range(3):
-    job_1 = Job(f"job{i + 1}_1")
-    model.addJob(job_1)
-    mode_1 = Mode(f"mode{i + 1}_1")
-    mode_1.addResource("machine1", np.array([1 for j in range(3 - i)]))
-    if i != 2:
-        mode_1.addResource("dummy_machine", np.array([0 for dm in range(3 - i)]))
-    model.addMode(f"job{i + 1}_1", mode_1)
-
-    job_2 = Job(f"job{i + 1}_2")
-    model.addJob(job_2)
-    mode_2 = Mode(f"mode{i + 1}_2")
-    mode_2.addResource("machine2", np.array([1 for j in range(3 - i)]))
-    model.addMode(f"job{i + 1}_2", mode_2)
+jb_dict = {(1, 1): 1, (1, 2): 2, (2, 1): 2, (2, 2): 1, (3, 1): 2, (3, 2): 1}
+for key, value in jb_dict.items():
+    job = Job(f"job{key[0]}_{key[1]}")
+    model.addJob(job)
+    mode = Mode(f"mode{key[0]}_{key[1]}")
+    mode.addResource(f"machine{value}", np.array([1 for r in range(4 - key[0])]))
+    if key == (1, 1) or key == (2, 1):
+        mode.addResource("dummy_machine", np.array([0 for j in range(4 - key[0])]))
+    mode.setDuration(4 - key[0])
+    model.addMode(f"job{key[0]}_{key[1]}", mode)
 
 # setup job
 setup_job = Job("setup_job3_1")
 model.addJob(setup_job)
 setup_mode = Mode("setup_mode3_1")
 setup_mode.addResource("machine2", np.array([1]))
+setup_mode.setDuration(1)
 model.addMode("setup_job3_1", setup_mode)
 model.setSetupMode("setup_job3_1", None, ["setup_mode3_1"])
 
@@ -42,13 +39,18 @@ model.setSetupMode("setup_job3_1", None, ["setup_mode3_1"])
 dummy_job = Job("dummy")
 model.addJob(dummy_job)
 dummy_mode = Mode("dummy_mode")
-dummy_mode.addResource("dummy_machine", np.array([0]))
+dummy_mode.addResource("dummy_machine", np.array([]))
+dummy_mode.setDuration(0)
 model.addMode("dummy", dummy_mode)
 
 # sink
 sink = Job("sink")
 model.addJob(sink)
 sink_mode = Mode("sink")
+sink_mode.addResource("machine1", np.array([]))
+sink_mode.addResource("machine2", np.array([]))
+sink_mode.addResource("dummy_machine", np.array([]))
+sink_mode.setDuration(0)
 model.addMode("sink", sink_mode)
 
 # precedence constraint
@@ -60,3 +62,8 @@ for i in range(1, 4):
 model.addImmediatePrecedence("dummy", "job1_1", "dummy_machine")
 model.addImmediatePrecedence("dummy", "job2_1", "dummy_machine")
 model.addImmediatePrecedence("setup_job3_1", "job3_1", "machine2")
+
+job_list = ["setup_job3_1", "job3_1", "dummy", "job1_1", "job2_1", "job3_2", "job1_2", "job2_2", "sink"]
+mode_list = ["setup_mode3_1", "mode3_1", "dummy_mode", "mode1_1", "mode2_1", "mode3_2", "mode1_2", "mode2_2", "sink"]
+
+p = model.CONSTRUCT(job_list=job_list, mode_list=mode_list, max_t=15)
